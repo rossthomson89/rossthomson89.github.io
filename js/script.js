74c2ff97e5a0394c756b9b21f8c7a2febde7dcb4
rossfortytwo <Vxw74vkx>
2019-06-04 16:45:04 +0100

    Added some cool animations/added some tech logos/added jquery

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(function() {
+
+    //Fade in on scroll
+    $(document).on("scroll", function () {
+        var pageTop = $(document).scrollTop();
+        var pageBottom = pageTop + $(window).height();
+        var tags = $("section");
+
+        for (var i = 0; i < tags.length; i++) {
+            var tag = tags[i];
+
+            if ($(tag).position().top < pageBottom) {
+                $(tag).addClass("visible");
+            } else {
+                $(tag).removeClass("visible");
+            }
+        }
+    });
+
+    //Smooth page scroll for nav links
+    $('a[href*="#"]').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate(
+            {
+                scrollTop: $($(this).attr('href')).offset().top,
+            },500,'swing');
+    });
+
+
+});
