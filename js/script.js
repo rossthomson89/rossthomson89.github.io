29877be3e332c8437da945c85a3381bd46ef3415
rossfortytwo <ross@fortytwo.studio>
2019-08-10 21:49:18 +0100

    mobile nav tweaks

--- js/script.js
+++ js/script.js
@@ -39,10 +39,14 @@ $(function() {
         if ($(window).scrollTop() >= 20) {
             $('.nav_container').addClass('fixed_nav');
             $('main').addClass('margin_fix');
+            $('.mobile_nav input + label').addClass('burger_fix');
+            $('.mobile_nav nav').addClass('mobile_nav_fix');
         }
         else {
             $('.nav_container').removeClass('fixed_nav');
             $('main').removeClass('margin_fix');
+            $('.mobile_nav input + label').removeClass('burger_fix');
+            $('.mobile_nav nav').removeClass('mobile_nav_fix');
         }
     });
 
